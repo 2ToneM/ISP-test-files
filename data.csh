5fffb81cdb40d52fa090bc8096edf6f716bd61ea
2Tone <82159523+2ToneM@users.noreply.github.com>
2022-04-26 16:42:22 +0300

    Add files via upload

--- data.csh
+++ data.csh
@@ -1,8 +1,46 @@
-#!/bin/csh -f
-# Run this script to get the AGAGE MCF data
-rm ./*-gcmd.mon
-wget -r --no-parent -A '*-gcmd.mon' http://agage.eas.gatech.edu/data_archive/agage/gc-md/monthly/
-mv agage.eas.gatech.edu/data_archive/agage/gc-md/monthly/*-gcmd.mon ./.
-rm -rf agage.eas.gatech.edu
-exit(0)
+### Basic
+alias ..    'cd ..'
+alias cd..  'cd ..'
+#alias -    'cd -'
+alias cd-   'cd -'
+alias sl    ls
+alias rm    'rm -i'
+alias mv    'mv -i'
+alias cp    'cp -i'
+alias df    'df -h'
+alias du    'du -h'
+alias h     history
+alias p     ping
+alias grep  'grep --color=always'
+alias fgrep 'egrep --color=always'
+alias egrep 'fgrep --color=always'
+### ls
+alias ls    'ls -Fh --color=always'
+alias ll    'ls -l'
+alias la    'ls -A'
+alias lla   'ls -lA'
+### less
+alias less  'less -R -x4'
+alias l     'less'
+alias lsl   'ls | less'
+alias lll   'ls -l | less'
+alias llal  'ls -lA | less'
+alias lest  'last -a | less'
+alias hl    'history | less'
+
+### GNU screen
+alias s     screen
+alias sls   'screen -ls'
+alias sr    'screen -R'
+
+### Editor
+alias e     emacs
+alias enw   'emacs -nw'
+alias v     vim
+
+### Programming
+alias m     'matlab -nodisplay'
+
+### TeX
+# alias xdvi   pxdvi
 
--- data.sh
+++ data.sh
@@ -1,105 +1,26 @@
-#!/bin/bash
-# File Name:	data.sh
-# Author:	Key Zhang
-# Written:	7-20-2015
-# Purpose:  Data store function
-table="test.db"
-key="testkey"
-value="testv"
+#!/sbin/sh
 
-#dataselect <table name> <key> 
-function dataselect() 
-{
-    #get table name
-    if [[ -n $1 ]]; then
-        table=$1
-    fi
-    #key
-    if [[ -n $2 ]]; then
-        key=$2
-    fi
-    #get value
-    res=$(grep "\<$key\>" $table|cut -d'>' -f2|cut -d'<' -f1)
-    #check value
-    if [[  $res ]]; then
-        echo $res
-    else
-        return 1
-    fi
-}
+######## BootMenu Script
+######## Execute [USB Data Partition] Tool
 
-#datainsert <table name> <key> <value>
-function datainsert()
-{
-    #get table name
-    if [[ -n $1 ]]; then
-        table=$1
-    fi
-    #get key
-    if [[ -n $2 ]]; then
-        key=$2
-    fi
-    #get value
-    if [[ -n $3 ]]; then
-        value=$3
-    fi
-    res=$(grep "\<$key\>" $table|cut -d'>' -f2|cut -d'<' -f1)
-    if [[  ${#res}>1 ]]; then
-        return 1
-    else
-        #write record
-        echo "<$key>$value<>"|cat >>$table
-        #echo "Written: <$key>$value<>"
-        return 0
-    fi
-}
+source /system/bootmenu/script/_config.sh
 
-#dataupdate <table name> <key> <value>
-function dataupdate()
-{
-    if [[ -n $1 ]]; then
-        table=$1
-    fi
-    if [[ -n $2 ]]; then
-        key=$2
-    fi
-    if [[ -n $3 ]]; then
-        value=$3
-    fi
-    res=$(grep "\<$key\>" $table);
-    if [[  $res ]]; then
-        #replace record
-        sed -i "s/$res/<$key>$value<>/" $table
-        return 0;
-    else
-        #No matching record
-        return 1;
-    fi
-}
+######## Main Script
 
-#datadelete table key 
-function datadelete()
-{
-    if [[ -n $1 ]]; then
-        table=$1
-    fi
-    if [[ -n $2 ]]; then
-        key=$2
-    fi
-    res=$(grep "\<$key\>" $table);
-    if [[  $res ]]; then
-        #replace record
-        sed -i "/$res/d" $table
-        return 0;
-    else
-        #No matching record
-        return 1;
-    fi
-}
+# acm to disable MSC
+sync
+echo acm > /dev/usb_device_mode
+sleep 1
 
-#test
-U="USER.db"
-datadelete $U ID
-# id="3"
-# dataselect $U ${id}"_TYPE"
-# dataupdate $U ${id}"_NO" 333
+PARTITION=$PART_DATA
+
+echo $PARTITION > $BOARD_UMS_LUNFILE
+
+# charge_only support MSC
+echo charge_only > /dev/usb_device_mode
+echo usb_mode_charge > /tmp/usbd_current_state
+
+echo $PARTITION > $BOARD_UMS_LUNFILE
+
+exit
+
